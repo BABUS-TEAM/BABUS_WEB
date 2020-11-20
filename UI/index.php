7dee7ea2f1f3f67f3469d328a72013cfd4541904
nathyyo <46191581+nathyyo@users.noreply.github.com>
2020-11-20 11:10:06 +0300

    how it works section

--- UI/index.php
+++ UI/index.php
@@ -5,8 +5,81 @@
  include("components/header1.php");
  include("components/navigation&preloader.php");
 ?>
+ <!-- App Section Begin -->
+ <section class="app-section spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <img src="<?php echo $imglocation; ?>phone.png" alt="">
+                </div>
+                <div class="col-lg-6 offset-lg-1">
+                    <div class="app-text">
+                        <div class="section-title">
+                            <h2><?php echo $lang['60']; ?></h2>
+                            <p><?php echo $lang['61']; ?></p>
+                        </div>
+                        <p><?php echo $lang['62']; ?></p>
+                        <ul>
+                            <li><img src="<?php echo $imglocation; ?>check-icon.png" alt=""><?php echo $lang['63']; ?></li>
+                            <li><img src="<?php echo $imglocation; ?>check-icon.png" alt=""><?php echo $lang['64']; ?></li>
+                            <li><img src="<?php echo $imglocation; ?>check-icon.png" alt=""><?php echo $lang['65']; ?>
+                            </li>
+                        </ul>
+                        <a href="#"><img src="<?php echo $imglocation; ?>apple-store.jpg" alt=""></a>
+                        <a href="#"><img src="<?php echo $imglocation; ?>google-store.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- App Section End -->
+    
 
+    <!-- How It Works Section Begin -->
+    <section class="work-section set-bg" data-setbg="<?php echo $imglocation; ?>line-bg.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2><?php echo $lang['66']; ?></h2>
+                        <p><?php echo $lang['67']; ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="single-work-item">
+                        <div class="number">01.</div>
+                        <div class="work-text">
+                            <h4><?php echo $lang['68']; ?></h4>
+                            <p><?php echo $lang['69']; ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="single-work-item">
+                        <div class="number">02.</div>
+                        <div class="work-text">
+                            <h4><?php echo $lang['70']; ?></h4>
+                            <p><?php echo $lang['71']; ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="single-work-item">
+                        <div class="number">03.</div>
+                        <div class="work-text">
+                            <h4><?php echo $lang['72']; ?></h4>
+                            <p><?php echo $lang['73']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- How It Works Section End -->
 
+   
     <!-- Testimonial Section Begin -->
     <section class="testimonial-section">
         <div class="container">
