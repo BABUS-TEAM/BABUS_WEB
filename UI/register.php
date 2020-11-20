f1c487fc9eaea25bb4237f513d0e3f498606fe5a
nathyyo <46191581+nathyyo@users.noreply.github.com>
2020-11-20 12:30:38 +0300

    registration UI

---
+++ UI/register.php
@@ -0,0 +1,14 @@
+<?php
+ $title="register";
+ $page="register";
+ include("../config/config3.php");
+ include("components/header1.php");
+ include("components/navigation&preloader.php");
+?>
+
+   
+<?php
+    include("components/footer1.php");
+    include("components/ending1.php");
+?>
+
